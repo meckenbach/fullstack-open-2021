6039be422380e6956f0531c80db7255648bf85eb
meckenbach <mle9@student.london.ac.uk>
2021-05-12 16:40:45 +0200

    Refactor

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const App = () => {
-  const course = 'Half Stack application development'
+const Header = ({course}) => {
+  return (
+      <h1>{course}</h1>
+  )
+}
+
+const Content = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
@@ -11,17 +16,37 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
+
+const Total = ({number}) => {
+  return (
+    <p>Number of exercises {number}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total number={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }

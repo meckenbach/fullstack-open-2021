5bb6394a081ee4b4aa0cf33ea661961640a26b83
meckenbach <mle9@student.london.ac.uk>
2021-05-13 12:13:21 +0200

    Refactor buttons

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -40,6 +40,12 @@ const Statistics = ({good, neutral, bad}) => {
   )
 }
 
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -50,9 +56,9 @@ const App = () => {
     <div>
       <h1>Give Feedback</h1>
       <div>
-        <button onClick={() => setGood(good+1)}>good</button>
-        <button onClick={() => setNeutral(neutral+1)}>neutal</button>
-        <button onClick={() => setBad(bad+1)}>bad</button>
+        <Button onClick={() => setGood(good+1)} text="good" />
+        <Button onClick={() => setNeutral(neutral+1)} text="neutral" />
+        <Button onClick={() => setBad(bad+1)} text="bad" />
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
       

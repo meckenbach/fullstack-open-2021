fa081bb84fa02bbfbf982e216cec8cbed5aff667
meckenbach <mle9@student.london.ac.uk>
2021-05-24 17:52:54 +0200

    exc7.5

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -83,6 +83,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset =  (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -100,6 +107,7 @@ const CreateNew = (props) => {
           <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )

01d48bd635cf1d1581198a623cb4c195c607cba7
meckenbach <mle9@student.london.ac.uk>
2021-05-24 17:49:50 +0200

    exc7.4

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    event.preventDefault()
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}

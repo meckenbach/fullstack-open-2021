5dbf552e7436857d9f8130ee04a5f8ec9bc2194d
meckenbach <mle9@student.london.ac.uk>
2021-05-26 18:48:09 +0200

    add status of async request to store

--- part7/bloglist-2.0/client/src/reducers/usersReducer.js
+++ part7/bloglist-2.0/client/src/reducers/usersReducer.js
@@ -1,20 +1,40 @@
 import usersService from '../services/users'
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-  case 'INIT_USERS':
-    return action.users
+const initialState = {
+  users: [],
+  status: 'idle',
+  error: ''
+}
+
+const reducer = (state = initialState, { type, users, error }) => {
+  switch (type) {
+  case 'GET_USERS_PENDING':
+    return { ...state, staus: 'loading' }
+  case 'GET_USERS_FULFILLED':
+    return { ...state, status: 'success', users }
+  case 'GET_USERS_REJCTED':
+    return { ...state, status: 'failure', error }
   default:
     return state
   }
 }
 
 export const initializeUsers = () => async (dispatch) => {
-  const users = await usersService.getAll()
   dispatch({
-    type: 'INIT_USERS',
-    users
+    type: 'GET_USERS_PENDING'
   })
+  try {
+    const users = await usersService.getAll()
+    dispatch({
+      type: 'GET_USERS_FULFILLED',
+      users
+    })
+  } catch (error) {
+    dispatch({
+      type: 'GET_USERS_REJCTED',
+      error: error.response.data.error
+    })
+  }
 }
 
 export default reducer

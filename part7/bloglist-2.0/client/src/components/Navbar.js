4442cdf8fbe96131bcc7a7f87a38c5c3cb3f918d
meckenbach <mle9@student.london.ac.uk>
2021-05-26 22:37:28 +0200

    ex7.16

--- part7/bloglist-2.0/client/src/components/Navbar.js
+++ part7/bloglist-2.0/client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 import { logoutUser } from '../reducers/userReducer'
 
 const selectUser = (state) => state.user
@@ -16,6 +17,10 @@ const Navbar = () => {
 
   return (
     <nav>
+      <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
+        <li style={{ display: 'inline-block', paddingRight: 5 }}><NavLink activeStyle={{ fontWeight: 'bold' }} to="/blogs">blogs</NavLink></li>
+        <li style={{ display: 'inline-block', paddingRight: 5 }}><NavLink activeStyle={{ fontWeight: 'bold' }} to="/users">users</NavLink></li>
+      </ul>
       <p>{loggedInUser.name} logged in<button onClick={handleLogout}>logout</button></p>
     </nav>
   )

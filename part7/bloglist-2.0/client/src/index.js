d1820175a2bf8863068aba9d35e0e33702a6df9d
meckenbach <mle9@student.london.ac.uk>
2021-05-28 13:10:23 +0200

    add global styles

--- part7/bloglist-2.0/client/src/index.js
+++ part7/bloglist-2.0/client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import App from './App'
+import GlobalStyle from './GlobalStyle'
 import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   document.getElementById('root')

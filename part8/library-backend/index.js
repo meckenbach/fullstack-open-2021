c3c96f4497bbc82ff918e0593f61fed1882c542a
meckenbach <mle9@student.london.ac.uk>
2021-05-28 17:41:41 +0200

    ex 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => args ? books.filter(book => book.author === args.author) : books,
     allAuthors: () => {
       return authors.map(author => {
         const bookCount = books

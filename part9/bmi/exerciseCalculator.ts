f39d6b3efc6aa9dfd3e8c7693a09bb829dc5d96a
meckenbach <mle9@student.london.ac.uk>
2021-06-01 11:17:53 +0200

    add error handling

--- part9/bmi/exerciseCalculator.ts
+++ part9/bmi/exerciseCalculator.ts
@@ -21,7 +21,7 @@ const calculateExercises = (dailyExerciseHours: number[], target: number): Resul
     .filter(day => day !== 0)
     .length;
 
-  const average = dailyExerciseHours.reduce((acc, n) => acc + n, 0) / periodLength;
+  const average = periodLength === 0 ? 0 : dailyExerciseHours.reduce((acc, n) => acc + n, 0) / periodLength;
 
   const success = average >= target;
 
@@ -48,5 +48,30 @@ const calculateExercises = (dailyExerciseHours: number[], target: number): Resul
   };
 
 };
-const [,, target, ...dailyExerciseHours] = process.argv;
-console.log(calculateExercises(dailyExerciseHours.map(hours => +hours), +target));
+
+const parseArguments = (args: string[]): [number[], number] => {
+  if (args.length < 3) throw new Error('not enough arguments; at least provide a target value');
+
+  const target: number = +args[2];
+  if (isNaN(target)) throw new Error('provided argument "target" is not a number');
+
+  const dailyExerciseHours: number[] = args
+    .slice(3)
+    .map(hours => {
+      if (isNaN(+hours)) throw new Error('provided arguments "dailyExerciseHours" must be numbers');
+      return +hours;
+    });
+  
+  return [
+    dailyExerciseHours,
+    target
+  ];
+};
+
+try {
+  const [dailyExerciseHours, target] = parseArguments(process.argv);
+  console.log(calculateExercises(dailyExerciseHours, target));
+} catch (error) {
+  console.log('Error:', error.message);
+}
+

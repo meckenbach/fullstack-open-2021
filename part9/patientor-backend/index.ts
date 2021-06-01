cb39ea9146eed80e65ed4228e7dad143c4d5583b
meckenbach <mle9@student.london.ac.uk>
2021-06-01 15:57:36 +0200

    ex 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
 });
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.status(200).send('pong');
 });

61b95a25143b2a550d073adb0cc63ecc25e1d858
meckenbach <mle9@student.london.ac.uk>
2021-06-02 10:52:25 +0200

    ex 9.17

---
+++ part9/patientor-frontend/src/PatientPage/index.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React from "react";
+import { Container, Header, Icon, Loader, SemanticICONS  } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
+import { useStateValue } from "../state";
+
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+
+  const [{ patient }, dispatch] = useStateValue();
+
+  React.useEffect(() => {
+    const fetchPatient = async () => {
+      const { data: patientFromApi } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+      dispatch({ type: "SET_PATIENT", payload: patientFromApi});
+    };
+    if (!patient || patient.id !== id) void fetchPatient();
+  });
+
+  if (patient === null) return <Loader>Loading</Loader>;
+
+  const iconName = {
+    "male": "mars",
+    "female": "venus",
+    "other": "genderless"
+  }[patient.gender] as SemanticICONS;
+
+  return (
+    <Container>
+      <Header as="h2">{patient.name}<Icon name={iconName} /></Header>
+      <Container>
+        <div>ssn: {patient.ssn}</div>
+        <div>occupation: {patient.occupation}</div>
+      </Container>
+    </Container>
+  );
+};
+
+export default PatientPage;
